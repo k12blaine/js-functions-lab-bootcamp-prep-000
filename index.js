2a647c770b2289c002cac737acf70f009deb99dc
Kiana Blaine <kiana.blaine1@gmail.com>
2019-02-10 01:28:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays()
+{
+  var string = "Happy holidays!"
+  return string
+}
+
+function happyHolidaysTo(name)
+{
+  var string = `"Happy holidays, ${name}!"
+  return string
+}
