461c4c92c22e4f3c1915cbbb202bacd15976e187
Kiana Blaine <kiana.blaine1@gmail.com>
2019-02-10 01:31:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ function happyHolidays()
   var string = "Happy holidays!"
   return string
 }
-
 function happyHolidaysTo(name)
 {
   var string = `Happy holidays, ${name}!`
   return string
 }
-
 function happyHolidayTo(holiday, name)
 {
   var string = `Happy ${holiday}, ${name}!`
+  return string
+}
+function holidayCountdown(days, holiday)
+{
+  var string = `It's ${days} days until ${holiday}!`
   return string
 }
